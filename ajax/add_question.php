bdd455f45cf0d447c7bdda4795b14234de6d1582
Thomas Feldmann <feldmann.thomas@googlemail.com>
2013-02-21 13:10:04 +0100

    converted tabs to spaces

--- ajax/add_question.php
+++ ajax/add_question.php
@@ -7,15 +7,15 @@ $question = $_POST['question'];
 $answer = $_POST['answer'];
 
 $model = new Model;
-$s = $model->add_question($category, $question,	$answer);
+$s = $model->add_question($category, $question, $answer);
 
 if ($s)
 {
-	$response = array("done"=>true, "message"=>"");
+    $response = array("done"=>true, "message"=>"");
 }
 else
 {
-	$response = array("done"=>false, "message"=>"Es ist ein Fehler aufgetreten.$category-$question-$answer");
+    $response = array("done"=>false, "message"=>"Es ist ein Fehler aufgetreten.$category-$question-$answer");
 }
 
 print_r(json_encode($response));
--- model.php
+++ model.php
@@ -4,71 +4,71 @@ require_once("prevent_injections.php");
 
 class Model
 {
-	/**
-	 * Setup database connection
-	 */
-	function __construct()
-	{
-		include_once("config.php");
-		$mysqlConnect = mysql_connect($db_loc, $db_user, $db_pass) or die(mysql_error());
-		mysql_select_db($db_name) or die(mysql_error());
-		mysql_query('set character set utf8;') or die(mysql_error());
-	}
+    /**
+     * Setup database connection
+     */
+    function __construct()
+    {
+        include_once("config.php");
+        $mysqlConnect = mysql_connect($db_loc, $db_user, $db_pass) or die(mysql_error());
+        mysql_select_db($db_name) or die(mysql_error());
+        mysql_query('set character set utf8;') or die(mysql_error());
+    }
 
 
-	/**
-	 * Adds a question. If the chosen category doesn't exist it will be
-	 * created.
-	 * @param [type] $category [description]
-	 * @param [type] $question [description]
-	 * @param [type] $answer   [description]
-	 */
-	function add_question($category, $question, $answer)
-	{
-		// check if category already exists
-		$query = mysql_query("SELECT id
-			                    FROM categories
-			                   WHERE name = $category
-			                   LIMIT 1");
-		if (!$query)
-		{
-			$category_id = $this->add_category($category);
-		}
-		else
-		{
-			$id = mysql_fetch_array($query);
-			if (!$id) return false;
-			$category_id = $id[0];
-		}
+    /**
+     * Adds a question. If the chosen category doesn't exist it will be
+     * created.
+     * @param [type] $category [description]
+     * @param [type] $question [description]
+     * @param [type] $answer   [description]
+     */
+    function add_question($category, $question, $answer)
+    {
+        // check if category already exists
+        $query = mysql_query("SELECT id
+                                FROM categories
+                               WHERE name = $category
+                               LIMIT 1");
+        if (!$query)
+        {
+            $category_id = $this->add_category($category);
+        }
+        else
+        {
+            $id = mysql_fetch_array($query);
+            if (!$id) return false;
+            $category_id = $id[0];
+        }
 
-		return true;
-	}
-
-
-	/**
-	 * List all available categories
-	 * @return array list of all categories
-	 */
-	function list_categories()
-	{
-		$categories = array();
-		$query = mysql_query("SELECT name FROM categories");
-		while ($category = mysql_fetch_array($query))
-		{
-			$categories[] = $category['name'];
-		}
-		return $categories;
-	}
+        return true;
+    }
 
 
-	/**
-	 * Add a category
-	 * @param string $name name of new category
-	 * @return id of inserted category
-	 */
-	function add_category($name)
-	{
-		$query = mysql_query("INSERT INTO categories SET name = '$name'");
-		return mysql_insert_id();
-	}
+    /**
+     * List all available categories
+     * @return array list of all categories
+     */
+    function list_categories()
+    {
+        $categories = array();
+        $query = mysql_query("SELECT name FROM categories");
+        while ($category = mysql_fetch_array($query))
+        {
+            $categories[] = $category['name'];
+        }
+        return $categories;
+    }
+
+
+    /**
+     * Add a category
+     * @param string $name name of new category
+     * @return id of inserted category
+     */
+    function add_category($name)
+    {
+        $query = mysql_query("INSERT INTO categories SET name = '$name'");
+        return mysql_insert_id();
+    }
 }
